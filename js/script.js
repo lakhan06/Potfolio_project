54b96a176495ba85281591f715ef40a9b058129a
Chinmay Jain <chinmayluhadiya62@gmail.com>
2023-11-10 00:14:50 +0530

    final-changes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
-var sidebarEl=document.querySelector(".sidebar")
-var toogleEl=document.querySelector(".toogle")
+var sidebarEl = document.querySelector(".sidebar");
+var toogleEl = document.querySelector(".toogle");
 // var sidebarEl=document.querySelector(".sidebar")
 
-
-
-
-
-function sidebarcomes(){
-   sidebarEl.style.setProperty("transform", "translateY(0px)")
-   toogleEl.style.setProperty("opacity", "0")
+function sidebarcomes() {
+  sidebarEl.style.setProperty("transform", "translateY(0px)");
+  toogleEl.style.setProperty("opacity", "0");
 }
-function sidebargo(){
-   sidebarEl.style.setProperty("transform", "translateY(-2000px)")
-   toogleEl.style.setProperty("display", "block")
+function sidebargo() {
+  sidebarEl.style.setProperty("transform", "translateY(-2000px)");
+  toogleEl.style.setProperty("display", "block");
 }
