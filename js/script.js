8418b2a32975391f3a6d4f313ac79c4c1a0136ae
Lakhan Gupta <2021pcecslakhan090@poornima.org>
2023-11-09 12:49:57 +0530

    change

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var sidebarEl=document.querySelector(".sidebar")
+
+
+
+
+function sidebarcome(){
+   sidebarEl.style.setProperty("transform", "translateY(0)")
+   
+}
+function sidebargo(){
+   sidebarEl.style.setProperty("transform", "translateY(-2000px)")
+   
+}
